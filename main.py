2ccd23aa3c5ad9fab2902eb9d68f8a6e9fbfc767
vohai2003 <63694226+vohai2003@users.noreply.github.com>
2024-01-19 21:56:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 if not os.path.isdir("./OANC"):
-  os.system("nix install unzip")
   os.system("curl https://www.anc.org/OANC/OANC-1.0.1-UTF8.zip -o OANC-1.0.1-UTF8.zip")
   os.system("unzip OANC-1.0.1-UTF8.zip")
 import bptokenizer
